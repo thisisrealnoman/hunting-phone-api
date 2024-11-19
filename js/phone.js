94a4d396461acf6e33dda53c8373c5eb46cad1b2
thisisrealnoman <nomanontheroad@gmail.com>
2024-11-19 22:37:33 +0600

    added api

---
+++ js/phone.js
@@ -0,0 +1,7 @@
+const loadPhone = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    console.log(data);
+}
+
+loadPhone();
