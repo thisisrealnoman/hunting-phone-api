948d242f7a4fe42cdcc234df43d737cfd737b9eb
thisisrealnoman <nomanontheroad@gmail.com>
2024-11-21 22:18:53 +0600

    .

--- js/phone.js
+++ js/phone.js
@@ -2,7 +2,23 @@
 const loadPhone = async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
+    displayPhones(data);
+}
+
+// const displayPhones = phones => {
+//     // console.log(phones);
+//     phones.forEach(phone => {
+//         console.log(phone);
+//     });
+// }
+
+const displayPhones = phones => {
+    phones.forEach(phone => {
+        console.log(phone);
+    });
 }
 
 loadPhone();
+
+// trying to find the error
